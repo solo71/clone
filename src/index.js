e6e8ba22b37eaaed295080d3a2f0a29f13c6c549
Владимир Никитин <solo77.job@gmail.com>
2022-02-15 21:52:17 +0300

    TopBar + css

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from 'routes_module'
 import { BrowserRouter as Router } from 'react-router-dom'
+import TopBar from 'components/topBar'
 
 const App = () => {
   return (
     <div>
-      <h3>Clone page</h3>
       <Router>
+        <TopBar />
         <Routes />
       </Router>
     </div>
